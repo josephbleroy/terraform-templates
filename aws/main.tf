ee349eb03daa6e46ec71a22f256f1a2bfadd4793
Joseph <joseph@josephbleroy.com>
2017-11-02 10:51:25 +0900

    Fixed syntax

--- aws/main.tf
+++ aws/main.tf
@@ -9,12 +9,12 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami                     = "ami-40d28157"
   instance_type           = "t2.micro"
-  vpc_security_group      = ["${aws_security_group.instance.id}"]
+  vpc_security_group_ids  = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "${var.server_port}" &
+              nohup busybox httpd -f -p 8080 &
               EOF
   tags {
       Name = "web-1"
